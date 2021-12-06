734e9bae1eff6ad0221817dbb694c1dcb77c17b2
Julien SAGUEZ <jsaguez@e2r11p18.clusters.42paris.fr>
2021-12-06 15:58:42 +0100

    Begin typedef for vector

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,4 +1,21 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include <iostream>
+#include <memory>
+
+namespace ft
+{
+	template < class T, class Allocator = std::allocator<T> >
+	class vector
+	{
+		typedef T											value_type;
+		typedef Allocator									allocator_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+	}
+}
+
 #endif
