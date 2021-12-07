59c2ba06b7217c763e8b87112491a087d9ebb4b8
Hayaks <julien.sag36@gmail.com>
2021-12-07 10:31:20 +0100

    clang

--- src/vector.hpp
+++ src/vector.hpp
@@ -6,16 +6,16 @@
 
 namespace ft
 {
-	template < class T, class Allocator = std::allocator<T> >
-	class vector
-	{
-		typedef T											value_type;
-		typedef Allocator									allocator_type;
-		typedef typename allocator_type::reference			reference;
-		typedef typename allocator_type::const_reference	const_reference;
-		typedef typename allocator_type::pointer			pointer;
-		typedef typename allocator_type::const_pointer		const_pointer;
-	}
-}
+    template <class T, class Allocator = std::allocator<T>>
+    class vector
+    {
+        typedef T                                        value_type;
+        typedef Allocator                                allocator_type;
+        typedef typename allocator_type::reference       reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer         pointer;
+        typedef typename allocator_type::const_pointer   const_pointer;
+    }
+} // namespace ft
 
 #endif
